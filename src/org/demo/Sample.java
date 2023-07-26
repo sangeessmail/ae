9136e61f9cbe5d494c9cdb991918ac08b8dde10d
sangeessmail <sangeessmail@gmail.com>
2023-07-26 13:11:08 +0530

    msg3

--- src/org/demo/Sample.java
+++ src/org/demo/Sample.java
@@ -1,6 +1,10 @@
 package org.demo;
 
 public class Sample {
+	private void m3() {
+		// TODO Auto-generated method stub
+System.out.println("msg3");
+	}
 public static void main(String[] args) {
 	System.out.println("message");
 }
